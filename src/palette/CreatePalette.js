ec18c9329a9fd19c770bdf639de1e649d3a9a3b4
Agnieszka Klobus <agnieszka.klobus@qiagen.com>
2024-02-02 12:32:28 +0100

    Move picker

--- src/palette/CreatePalette.js
+++ src/palette/CreatePalette.js
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './CreatePalette.css';
 import "../App.css"
 import PaletteSegment from "./paletteSegment/PaletteSegment";
+import Pickr from "@simonwep/pickr";
+import '@simonwep/pickr/dist/themes/classic.min.css';
 
 const CreatePalette = () => {
+    const colorPickerRef = useRef();
+
+    useEffect(() => {
+        const pickr = Pickr.create({
+            el: colorPickerRef.current,
+            theme: 'classic',
+            useAsButton: false,
+            position: 'bottom-middle',
+            showAlways: true,
+            components: {
+                preview: true,
+                opacity: false,
+                hue: true,
+                interaction: {
+                    input: true,
+                    clear: false,
+                    save: true,
+                },
+            },
+        });
+
+        return () => {
+            pickr.destroy();
+        };
+    }, [colorPickerRef]);
+
     return (
         <div className="rootComponent">
             <div>
                 <h2>CreatePalette</h2>
                 <p>Welcome to the home page!</p>
-                <PaletteSegment/>
             </div>
+            <div className="paletteSegment">
+                <table>
+                    <tbody>
+                    <tr>
+                        <td><h2>Pick base color</h2></td>
+                    </tr>
+                    <tr>
+                        <td className="colorPickerCell" rowSpan={3}>
+                            <div className="colorPicker" ref={colorPickerRef}></div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+            <PaletteSegment/>
         </div>
     );
 };
+
 
 export default CreatePalette;
 
--- src/palette/paletteSegment/PaletteSegment.js
+++ src/palette/paletteSegment/PaletteSegment.js
@@ -1,55 +1,24 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import '../CreatePalette.css';
-import '@simonwep/pickr/dist/themes/monolith.min.css';
-import Pickr from '@simonwep/pickr';
-
 
 const PaletteSegment = () => {
-    const colorPickerRef = useRef();
-
-    useEffect(() => {
-        const pickr = Pickr.create({
-            el: colorPickerRef.current,
-            theme: 'monolith',
-            useAsButton: false,
-            position: 'bottom-middle',
-            showAlways: true,
-            components: {
-                preview: true,
-                opacity: false,
-                hue: true,
-                interaction: {
-                    input: true,
-                    clear: false,
-                    save: true,
-                },
-            },
-        });
-
-        return () => {
-            pickr.destroy();
-        };
-    }, [colorPickerRef]);
 
     return (
         <div className="paletteSegment">
+            <h2>Color Palette</h2>
             <table>
                 <tbody>
                 <tr>
-                    <td><h2>Pick base color</h2></td>
-                    <td><h2>Palette</h2></td>
+                    <td>Color 1</td>
+                    <td><input type={"color"}/></td>
                 </tr>
                 <tr>
-                    <td className="colorPickerCell" rowSpan={3}>
-                        <div className="colorPicker" ref={colorPickerRef}></div>
-                    </td>
-                    <td><h2>Color</h2></td>
+                    <td>Color 2</td>
+                    <td><input type={"color"}/></td>
                 </tr>
                 <tr>
-                    <td><h2>Color</h2></td>
-                </tr>
-                <tr>
-                    <td><h2>Color</h2></td>
+                    <td>Color 3</td>
+                    <td><input type={"color"}/></td>
                 </tr>
                 </tbody>
             </table>
